653707050784f35cd77418ab9bc2bdfd7c202de8
NDDEVGAME <111946313+NDDEVVlog@users.noreply.github.com>
2024-05-07 23:41:49 +0700

    Lab3 Commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,122 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Button1_Pin GPIO_PIN_13
+#define Button1_GPIO_Port GPIOC
+#define Button2_Pin GPIO_PIN_1
+#define Button2_GPIO_Port GPIOA
+#define Button3_Pin GPIO_PIN_2
+#define Button3_GPIO_Port GPIOA
+#define Display10_Pin GPIO_PIN_3
+#define Display10_GPIO_Port GPIOA
+#define Display1_Pin GPIO_PIN_4
+#define Display1_GPIO_Port GPIOA
+#define LED_RED_Pin GPIO_PIN_5
+#define LED_RED_GPIO_Port GPIOA
+#define LED_YELLOW_Pin GPIO_PIN_6
+#define LED_YELLOW_GPIO_Port GPIOA
+#define LED_GREEN_Pin GPIO_PIN_7
+#define LED_GREEN_GPIO_Port GPIOA
+#define PB0_Pin GPIO_PIN_0
+#define PB0_GPIO_Port GPIOB
+#define PB1_Pin GPIO_PIN_1
+#define PB1_GPIO_Port GPIOB
+#define PB2_Pin GPIO_PIN_2
+#define PB2_GPIO_Port GPIOB
+#define LED_REDB_Pin GPIO_PIN_8
+#define LED_REDB_GPIO_Port GPIOA
+#define LED_YELLOWB_Pin GPIO_PIN_9
+#define LED_YELLOWB_GPIO_Port GPIOA
+#define LED_GREENB_Pin GPIO_PIN_10
+#define LED_GREENB_GPIO_Port GPIOA
+#define PB3_Pin GPIO_PIN_3
+#define PB3_GPIO_Port GPIOB
+#define PB4_Pin GPIO_PIN_4
+#define PB4_GPIO_Port GPIOB
+#define PB5_Pin GPIO_PIN_5
+#define PB5_GPIO_Port GPIOB
+#define PB6_Pin GPIO_PIN_6
+#define PB6_GPIO_Port GPIOB
+#define PB7_Pin GPIO_PIN_7
+#define PB7_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+typedef struct {
+    GPIO_TypeDef* GPIOx;
+    uint16_t GPIO_Pin;
+    int KeyReg0;
+    int KeyReg1;
+    int KeyReg2;
+    int KeyReg3;
+    int TimeOutForKeyPress;
+    int button1_flag;
+} Button;
+
+extern Button button1; // Declare a variable of type Button
+extern Button button2; // Declare a variable of type Button
+extern Button button3; // Declare a variable of type Button
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
